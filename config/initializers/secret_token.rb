8c818e66381004ad813021910ba65db78b1baa1f
Ahmed ElShamy <elshamy1st@gmail.com>
2013-01-03 09:00:08 +0300

    Task A: Creating the Application \ Iteration A1: Creating the Products Maintenance Application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'c54987cd5483d08171105e16fd314ad7f6020d055f2d2ab4cf4f7f2b6820371155a8a1cac5e913b34b8e8778a5e62ed08fc8b9dd1afab27cd5a048c0bb5ba80b'
